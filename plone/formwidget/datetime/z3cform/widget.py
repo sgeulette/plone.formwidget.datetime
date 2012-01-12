0a8cc68d734078ad84b065502df4e0c6f2147c3a
Lennart Regebro <regebro@gmail.com>
2012-01-12 14:34:53 +0100

    Let's have a property with the id of the js-updated field. That way we can use it from other fields when we need it (which we will in several instances).

--- plone/formwidget/datetime/z3cform/widget.py
+++ plone/formwidget/datetime/z3cform/widget.py
@@ -56,6 +56,12 @@ class DateWidget(base.AbstractDateWidget,
             return 'new Date(%s, %s, %s)' % (value_date)
         else:
             return ''
+            
+    @property
+    def js_field(self):
+        """Returns the id of a field that contains a js-parseable value of the selected date.
+        Used by plone.formwidget.recurrencewidget"""
+        return self.id + '-calendar'
 
 
 @zope.component.adapter(zope.schema.interfaces.IField, z3c.form.interfaces.IFormLayer)
