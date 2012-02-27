e9e8bd9f28b6005b65c3958fb0ef9b27a8327a1a
Johannes Raggam <raggam-nl@adm.at>
2012-02-27 22:57:30 +0100

    do direct imports

--- plone/formwidget/datetime/z3cform/interfaces.py
+++ plone/formwidget/datetime/z3cform/interfaces.py
@@ -1,42 +1,43 @@
-import z3c.form
-import z3c.form.interfaces
-import zope.schema
+from z3c.form.interfaces import IWidget
+from zope.schema import ValidationError
+from zope.schema import Bool
+from zope.schema.interfaces import IDate, IDatetime
 
 from plone.formwidget.datetime import MessageFactory as _
 
 
 # Fields
 
-class IDateField(zope.schema.interfaces.IDate):
+class IDateField(IDate):
     """ Special marker for date fields that use our widget """
 
-class IDatetimeField(zope.schema.interfaces.IDatetime):
+class IDatetimeField(IDatetime):
     """ Special marker for datetime fields that use our widget """
 
 
 # Widgets
 
-class IDateWidget(z3c.form.interfaces.IWidget):
+class IDateWidget(IWidget):
     """ Date widget marker for z3c.form """
 
-    show_today_link = zope.schema.Bool(
+    show_today_link = Bool(
         title=u'Show "today" link',
         description=(u'show a link that uses javascript to inserts '
                      u'the current date into the widget.'),
         default=False,
         )
 
-class IDatetimeWidget(z3c.form.interfaces.IWidget):
+class IDatetimeWidget(IWidget):
     """ Datetime widget marker for z3c.form """
 
-class IMonthYearWidget(z3c.form.interfaces.IWidget):
+class IMonthYearWidget(IWidget):
     """ MonthYear widget marker for z3c.form """
 
 
 # Errors
 
-class DateValidationError(zope.schema.ValidationError):
+class DateValidationError(ValidationError):
     __doc__ = _(u'Please enter a valid date.')
 
-class DatetimeValidationError(zope.schema.ValidationError):
+class DatetimeValidationError(ValidationError):
     __doc__ = _(u'Please enter a valid date and time.')
