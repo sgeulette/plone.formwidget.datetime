cbeb6f4e75fa778fe44c8055338d9392dc485be3
Johannes Raggam <raggam-nl@adm.at>
2012-10-12 12:31:30 +0200

    add test for new css resource

--- plone/formwidget/datetime/tests/test_resources.py
+++ plone/formwidget/datetime/tests/test_resources.py
@@ -12,12 +12,15 @@ class DatetimeTest(unittest.TestCase):
 
     def test_portal_js(self):
         # this plugin is disabled by default. let's check if it's now enabled'
-        js_id = "++resource++plone.app.jquerytools.dateinput.js"
         p_js = getToolByName(self.portal, 'portal_javascripts')
+        js_id = "++resource++plone.app.jquerytools.dateinput.js"
         self.failUnless(p_js.getResource(js_id).getEnabled())
 
     def test_portal_css(self):
         # this stylesheet is disabled by default. let's check if it's now enabled
-        css_id = "++resource++plone.app.jquerytools.dateinput.css"
         p_css = getToolByName(self.portal, 'portal_css')
+        css_id = "++resource++plone.app.jquerytools.dateinput.css"
+        self.failUnless(p_css.getResource(css_id).getEnabled())
+
+        css_id = "++resource++plone.formwidget.datetime/styles.css"
         self.failUnless(p_css.getResource(css_id).getEnabled())
