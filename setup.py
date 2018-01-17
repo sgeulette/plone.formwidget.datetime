33e819dcc59763ea6c18f20bb05b5e84694b6bd2
Maurits van Rees <maurits@vanrees.org>
2018-01-17 16:15:38 +0100

    Back to development: 1.3.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='plone.formwidget.datetime',
-    version='1.3.2',
+    version='1.3.3.dev0',
     description="Datetime widgets for Plone",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.rst").read()),
